abbb3e5622bc3bd19ba059f1f28924b605c10ab7
Benja <akiles.benja@hotmail.com>
2026-02-07 11:01:39 -0300

    Pagina etapa 2

--- codigo2.py
+++ codigo2.py
@@ -1,10 +1,22 @@
-def principal():
-    for i in range(5):
-        print("Hola", i)
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+        
+    def new_block(self):
+        # Creates a new Block and adds it to the chain
+        pass
+    
+    def new_transaction(self):
+        # Adds a new transaction to the list of transactions
+        pass
+    
+    @staticmethod
+    def hash(block):
+        # Hashes a Block
+        pass
 
-
-
-
-
-if __name__ == "__main__":
-    principal()
+    @property
+    def last_block(self):
+        # Returns the last Block in the chain
+        pass
